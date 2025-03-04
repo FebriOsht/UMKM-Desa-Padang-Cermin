5dffae55741f0873928029db392f6f71ae2edf1c
FebriOsht <fashter022@gmail.com>
2025-03-05 01:32:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+function toggleMenu() {
+    const menu = document.getElementById ('mobile-menu');
+    menu.classList.toggle('open'); // Menambahkan atau menghapus kelas "open" untuk menampilkan/menyembunyikan menu
+}
